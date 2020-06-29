a5cbcce3fb998df6b12c511b3f36b8e3b44f9f3c
nextwebb <oaikhenahpeterson@gmail.com>
2020-06-29 23:31:32 +0100

    added initial URLs

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
 from django.conf.urls import include
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('', include(router.urls)),
 ]
