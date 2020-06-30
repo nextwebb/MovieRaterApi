aab7a466ba38fc12a10466498542b4b316fb395c
nextwebb <oaikhenahpeterson@gmail.com>
2020-06-30 23:34:52 +0100

    Authentication

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MovieSerializers, RatingSerializers
@@ -9,14 +10,16 @@ from .serializers import MovieSerializers, RatingSerializers
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializers
+    authentication_classes = (TokenAuthentication,)
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            # user = request.user
-            user = User.objects.get(id=1)
+            user = request.user
+            print('user', user)
+            # user = User.objects.get(id=1)
 
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
@@ -25,6 +28,7 @@ class MovieViewSet(viewsets.ModelViewSet):
                 serializer = RatingSerializers(rating, many=False)
                 response = {'message': 'Rating  Updated', 'result': serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
+
             except:
                 rating = Rating.objects.create(user=user, movie=movie, stars=stars)
                 serializer = RatingSerializers(rating, many=False)
@@ -39,3 +43,4 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializers
+    authentication_classes = (TokenAuthentication,)
