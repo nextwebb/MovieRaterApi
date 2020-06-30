161b444991cb5bb7eabdd3675ab0f992fa86f1c4
nextwebb <oaikhenahpeterson@gmail.com>
2020-06-30 01:45:31 +0100

    custom method

--- api/views.py
+++ api/views.py
@@ -1,12 +1,17 @@
-
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializers import MovieSerializers, RatingSerializers
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = (MovieSerializers)
-
+    
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        response = {'message': 'its working'}
+        return Response(response, status=status.HTTP_200_OK)
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = (RatingSerializers)
