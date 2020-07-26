11a8283986b5be4993f8e92ea9d61cd8bfe4a8dc
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-26 22:48:02 +0100

    heroku deploy settings

--- movierater/settings.py
+++ movierater/settings.py
@@ -140,7 +140,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(prod_db)
 
 django_heroku.settings(locals())
