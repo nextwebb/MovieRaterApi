6154ada772b12d9d91646ead6ae1ba954a61cbdc
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-28 13:49:48 +0100

    enabling  whitenoise

--- movierater/settings.py
+++ movierater/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = [
--- movierater/wsgi.py
+++ movierater/wsgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movierater.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+# application = DjangoWhiteNoise(application)
