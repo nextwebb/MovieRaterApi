16e107e84b0f1329fbb42ef86e036479514fa860
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-28 13:16:22 +0100

    still tweaking for deploy

--- movierater/settings.py
+++ movierater/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 from decouple import config
-from dj_database_url import parse as dburl
+from dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
